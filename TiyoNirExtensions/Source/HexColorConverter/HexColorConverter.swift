e8e33b42316983d16429e516521c12d8e1f1d92b
Mounir Ybanez <rinuom91@gmail.com>
2018-01-04 17:13:58 +0800

    Checkpoint: Implementing HexColorConverter.

---
+++ TiyoNirExtensions/Source/HexColorConverter/HexColorConverter.swift
@@ -0,0 +1,13 @@
+//
+//  HexColorConverter.swift
+//  TiyoNirExtensions
+//
+//  Created by Mounir Ybanez on 1/4/18.
+//  Copyright © 2018 Nir. All rights reserved.
+//
+
+import UIKit
+
+public class HexColorConverter {
+
+}
